2f094977acd807844991483b44249ebd020998fa
Harsh Dadhich <hdadhich01@gmail.com>
2022-08-02 23:49:42 -0700

    Change os import

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
-from os.environ import get
+import os
 
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0', port = get('PORT'))
+    app.run(debug = True, host = '0.0.0.0', port = os.environ.get('PORT'))
