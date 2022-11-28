fd36de7ad42ef18048a863d0b87cef5daebad2e6
Harsh Dadhich <hdadhich01@gmail.com>
2022-11-28 12:14:54 -0800

    Work on weather bp

--- app/blueprints/weather/__init__.py
+++ app/blueprints/weather/__init__.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 from flask import redirect
 from flask import render_template
-
-from .helper import *
+from httpx import get
 
 weather = Blueprint("weather", __name__)
 
 
 @weather.route("/")
 def index():
+    req = get("https://wttr.in/Santa+Cruz?format=j1")
     return render_template("index.html", map=get_index())
